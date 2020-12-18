64c5a54cea20a2f90b7b2c50d1c5ac53fb4be315
Cesar Martinez <chernanma@msn.com>
2020-12-17 23:10:19 -0500

    Adding populate funtion to API route to pull last 7 workouts data

--- routes/api.js
+++ routes/api.js
@@ -29,7 +29,7 @@ router.put("/api/workouts/:id", (req, res) => {
 //API end point to pull all workout data 
 router.get("/api/workouts", (req, res) => {
   Workout.find({})
-    .sort({ date: -1 })
+    .sort({ date: -1 })  
     .then(dbWorkout => {
       res.json(dbWorkout);
     })
@@ -37,5 +37,20 @@ router.get("/api/workouts", (req, res) => {
       res.status(400).json(err);
     });
 });
+
+//API route to pull  last 7 workouts data 
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .sort({ date: -1 })
+    .limit(7)
+    .populate("exercises")
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
 
 module.exports = router;
